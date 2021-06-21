abd3d87e0c51d106418bb3f01b0760627f91cb9a
Steven Chen <schen192@my.centennialcollege.ca>
2021-06-21 17:42:25 -0400

    added mongodb uri

--- server/config/app.js
+++ server/config/app.js
@@ -7,16 +7,9 @@ let logger = require('morgan');
 
 // database setup
 let mongoose = require('mongoose');
-let DB = require('./db');
+let connectToMongoDB = require('./db');
 
-// point mongoose to the DB URI
-mongoose.connect(DB.URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-let mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-mongoDB.once('open', ()=>{
-  console.log('Connected to MongoDB...');
-});
 
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
@@ -40,19 +33,22 @@ app.use('/users', usersRouter);
 app.use('/book-list', booksRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: 'Error'});
+  res.render('error', { title: 'Error' });
 });
+
+// Connect to mongoDB
+connectToMongoDB();
 
 module.exports = app;
--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,14 @@
-/**
- * Add your code here to fix URI connection
- * 
- */
+const mongoose = require("mongoose");
+
+const connectToMongoDB = () => {
+    const URI = "mongodb+srv://schen196:Jcz4VhzPGXyLSW9n@cluster0.bu4ge.mongodb.net/midtermDB?retryWrites=true&w=majority";
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+    mongoose.connect(URI, options)
+        .then(() => console.log("Connected to MongoDB"))
+        .catch(error => console.log("Connection Error:", error));
+};
+
+module.exports = connectToMongoDB;
